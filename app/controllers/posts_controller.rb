1b27b35b25870f38aa78039b027a458f02f5f8f8
皆川千鶴 <minagawachizuru@minakawachidzurunoMacBook-Pro.local>
2023-01-15 21:09:59 +0900

    index,newページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,31 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :starrt_date, :end_date, :checkbox, :memo))
+    if @post.save
+      flash[:notice] = "予定を新規登録しました"
+      redirect_to("/posts/index")
+    else
+      render "new"
+   end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
